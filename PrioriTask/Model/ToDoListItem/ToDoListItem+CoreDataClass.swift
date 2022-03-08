f8e11ecf595e134d68a2169307ae70708415b58c
KevinJonathan117 <kevinjonathan3010@gmail.com>
2022-03-08 13:13:57 +0700

    Added basic CRUD function using Core Data

---
+++ PrioriTask/Model/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  PrioriTask
+//
+//  Created by Kevin Jonathan on 08/03/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ PrioriTask/Model/ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  PrioriTask
+//
+//  Created by Kevin Jonathan on 08/03/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
